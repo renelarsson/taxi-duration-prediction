c643b2bf8ece38b577d2e3b71192350ba357496d
René Larsson <dr.rlarsson@gmail.com>
2025-07-15 20:30:13 +0200

    Add type to all Lambda module variables

--- infrastructure/terraform/modules/kinesis/main.tf
+++ infrastructure/terraform/modules/kinesis/main.tf
@@ -4,9 +4,7 @@ resource "aws_kinesis_stream" "stream" {
   shard_count = var.shard_count
   retention_period = var.retention_period
   shard_level_metrics = var.shard_level_metrics
-  tags = {
-    CreatedBy = var.tags
-  }
+  tags = var.tags
 }
 
 output "stream_arn" {
--- infrastructure/terraform/modules/s3/main.tf
+++ infrastructure/terraform/modules/s3/main.tf
@@ -1,7 +1,11 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = var.bucket_name
-  acl = "private"
+  bucket        = var.bucket_name
   force_destroy = true
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
 }
 
 output "name" {
