88b937a8ba1342d74309b8d34d38879fc8d175be
renelarsson <renelarsson@users.noreply.github.com>
2025-08-01 12:32:49 +0200

    Fix: Terraform Docker build path for CI/CD

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -20,12 +20,12 @@ resource "null_resource" "ecr_image" {
 
   provisioner "local-exec" {
     command = <<EOF
-      aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${var.account_id}.dkr.ecr.${var.region}.amazonaws.com
-      docker build -f terraform/Dockerfile -t ${aws_ecr_repository.repo.repository_url}:${var.ecr_image_tag} ${path.root}
-      docker push ${aws_ecr_repository.repo.repository_url}:${var.ecr_image_tag}
-    EOF
-    working_dir = "${path.root}" # Set Docker build context to project root
+      aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${aws_ecr_repository.repo.repository_url}
+      docker build -f ../terraform/Dockerfile -t ${aws_ecr_repository.repo.repository_url}:latest ..
+      docker push ${aws_ecr_repository.repo.repository_url}:latest
+EOF
   }
+  depends_on = [aws_ecr_repository.repo]
 }
 
 # Get the image URI after pushing to ECR
